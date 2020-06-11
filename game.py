8cd0850fcc8c0989af8c8ed08ce3b289bce98dce
Jakub Feik <feikjakub@gmail.com>
2020-06-11 09:54:09 +0200

    Comments change according to PEP - 8

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 # Jakub Feik
 # 21/05/2020
 
-# imports
+# Imports.
 import time
 import random
 import pygame
@@ -11,23 +11,23 @@ import sys
 
 pygame.init()
 
-# window size
+# Window size setting
 screen_width = 800
 screen_height = 400
 
-# rectangle size and position
+# Rectangle size and position.
 xpos = 0
 ypos = 50
-rect_width = random.choice(range(screen_width-50))
+rect_width = random.choice(range(screen_width - 50))
 rect_height = 10
 rect_gap = 50
 
-# ball position and size
+# Ball position and size.
 radius = 10
 xcoor = random.choice(range(12, rect_width))
 ycoor = ypos - radius
 
-# colors
+# Color values.
 white = (255, 255, 255,)
 black = (0, 0, 0)
 red = (255, 0, 0)
@@ -37,28 +37,28 @@ light_blue = (78, 231, 245)
 dark_green = (37, 125, 0)
 
 
-# icon
+# Window icon.
 icon = pygame.image.load("ball.png")
 
-# window
+# Window parametres.
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Keep Falling")
 screen.fill(light_blue)
 pygame.display.set_icon(icon)
 
-# rectangles
+# Rectangle generation.
 for i in range(screen_height):
     pygame.draw.rect(screen, dark_green, (xpos, ypos, rect_width, rect_height))
     pygame.draw.rect(screen, dark_green, (rect_width + rect_gap, ypos,
                                           screen_width - (rect_width + rect_gap), rect_height))
     ypos += 50
-    rect_width = random.choice(range(screen_width-50))
+    rect_width = random.choice(range(screen_width - 50))
 
 
-# ball
+# Ball drawing.
 pygame.draw.circle(screen, white, (xcoor, ycoor), radius)
 
-# game loop
+# Game Loop.
 state = True
 while state:
     for event in pygame.event.get():
