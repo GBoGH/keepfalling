9f38c56f188bd4ac9d03d7ee2d5bcb6412990c5c
GBoGH <44493112+GBoGH@users.noreply.github.com>
2020-12-03 18:29:23 +0100

    databse connection and related functions

---
+++ db.py
@@ -0,0 +1,32 @@
+import mysql.connector as conn
+
+db = conn.connect(
+    host="localhost",
+    user="python",
+    password="python",
+    database="keepfalling"
+)
+
+
+def insert(table_name, UserName, LastScore, HighScore):
+    cursor = db.cursor()
+    querry = "INSERT INTO %s (UserName, LastScore, HighScore)" \
+             "VALUES (%s, %d, %d)" \
+             %(table_name, UserName, LastScore, HighScore)
+    cursor.execute(querry)
+    db.commit()
+
+def bestx(table_name, n):
+    cursor = db.cursor()
+    querry = "SELECT UserName, HighScore " \
+             "FROM %s " \
+             "ORDER BY HighScore DESC " \
+             "LIMIT %s" %(table_name, n)
+    cursor.execute(querry)
+    result = cursor.fetchall()
+    for i in result:
+        name = i[0]
+        name = name.replace("'", "")
+        score = i[1]
+        print("%s: %d"%(name, score))
+
