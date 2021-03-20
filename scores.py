70c3bab9dac03f599dc4314f042621d8559062e9
GBoGH <44493112+GBoGH@users.noreply.github.com>
2021-03-20 10:38:48 +0100

    changed few things

--- scores.py
+++ scores.py
@@ -6,11 +6,16 @@ def dictionary():
     scores = df.to_dict(orient="records")
     return scores
 
-def bestx(dict, n):
+def bestx(arr, n):
     output = ["HIGHSCORES:"]
-    for i in range(n):
-        name = dict[i].get("NAME")
-        score = dict[i].get("SCORE")
+
+    if len(arr) < n:
+        x = len(arr)
+    else:
+        x = n
+    for i in range(x):
+        name = arr[i].get("NAME")
+        score = arr[i].get("SCORE")
         output.append(f"{name}  {score}")
 
     return output
